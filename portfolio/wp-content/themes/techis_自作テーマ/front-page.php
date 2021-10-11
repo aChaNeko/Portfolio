5fce442556756076aca9ca2d03dbf1e80eff7e07
aChaNeko <chaneko914@icloud.com>
2021-10-11 16:44:49 +0900

    pc画面コーディング

--- portfolio/wp-content/themes/techis_自作テーマ/front-page.php
+++ portfolio/wp-content/themes/techis_自作テーマ/front-page.php
@@ -1,3 +1,121 @@
 <?php get_header(); ?>
- 
+
+<div class="section" id="about">
+    <div class="title">
+        <i class="ff_cursive">About</i>
+    </div>
+    <div class="about">
+        <div class="image">
+            <img src="<?php bloginfo('template_url'); ?>/img/baseball.jpg" alt="">
+        </div>
+        <div class="profile ff_serif">
+            <div class="pro-section">
+                <h1 class="pro-title">Name：</h1>
+                <h1>三ツ矢 昂平</h1>
+            </div>
+            <div class="pro-section">
+                <h1 class="pro-title">Birthday：</h1>
+                <h1>1999年9月14日</h1>
+            </div>
+            <div class="pro-section">
+                <h1 class="pro-title">Age：</h1>
+                <h1>22歳</h1>
+            </div>
+            <div class="pro-section">
+                <h1 class="pro-title">Interests：</h1>
+                <h1>野球/スノーボード</h1>
+            </div>
+            <div class="history ff_serif">
+                <h1 class="profile-title">History</h1>
+                <h1 class="pro-contents">2018年3月：高等学校(土木)卒業</h1>
+                <h1 class="pro-contents">2018年4月：自動車製造会社入社(勤務)</h1>
+                <h1 class="pro-contents">2021年3月：TECH.I.S入校(在学中)</h1>
+            </div>
+            <div class="skills ff_serif">
+                <h1 class="profile-title">Skills</h1>
+                <h1 class="pro-contents">HTML/CSS/JavaScript/PHP</h1>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="section" id="service">
+    <div class="title">
+        <i class="ff_cursive">Service</i>
+    </div>
+    <div class="service ff_serif">
+        <div class="price">
+            <h1>LPのコーディング代行：5000円〜</h1>
+            <i>※実績作りのため値下げ中です!!</i>
+            <h1>HPのコーディング代行：5000円〜</h1>
+            <i>※実績作りのため値下げ中です!!</i>
+            <p>JavaScript一点の実装につき +500円</p>
+            <p>お問い合わせフォームの実装 +500円</p>
+        </div>
+        <div class="service-contents">
+            <h1>ご対応内容</h1>
+            <p class="responsive">レスポンシブ対応(スマホ対応)</p>
+            <i>※ご希望の機種などあればお申し付けください。</i>
+            <p class="wordpress">Wordpress制作</p>
+            <p class="follow">長期アフターフォロー</p>
+            <i>※納品後1ヶ月間無料</i>
+        </div>
+        <div class="deadline">
+            <h1>納期</h1>
+            <p>作業量につき変動しますが、受注後1週間〜2週間ほど頂きます。</p>
+            <p>ご希望の納期がございましたらお申し付けください。</p>
+        </div>
+        <div class="cautions">
+            <h1>ご依頼にあたっての注意事項</h1>
+            <p>デザインカンプはphotoshop・Illustrator・XD・画像ファイルなんでも結構です。</p>
+            <p>サイトに使用する画像素材は全てお客様でご用意して頂きます。</p>
+            <p>その他ご不明点・ご要望ありましたらお気軽にお問い合わせください。</p>
+        </div>
+    </div>
+</div>
+<div class="section" id="works">
+    <div class="title">
+        <i class="ff_cursive">Works</i>
+    </div>
+    <div class="sub-section ff_serif">
+        <a href="">
+            <div class="result">
+                <img src="<?php bloginfo('template_url'); ?>/img/No-Image.png" alt="">
+                <p>丸富庵清月様</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="result">
+                <img src="<?php bloginfo('template_url'); ?>/img/No-Image.png" alt="">
+                <p>丸富庵清月様</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="result">
+                <img src="<?php bloginfo('template_url'); ?>/img/No-Image.png" alt="">
+                <p>丸富庵清月様</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="result">
+                <img src="<?php bloginfo('template_url'); ?>/img/No-Image.png" alt="">
+                <p>丸富庵清月様</p>
+            </div>
+        </a>
+    </div>
+</div>
+<div class="section" id="contact">
+    <div class="title">
+        <i class="ff_cursive">Contact</i>
+    </div>
+    <div class="contact">
+        <img src="<?php bloginfo('template_url'); ?>/img/IMG_1150.JPG" alt="">
+        <div class="sub-contact">
+            <p>名前：三ツ矢 昂平</p>
+            <p>メールアドレス：chaneko914@icloud.com</p>
+            <p>Instagram：@achaneko_914</p>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
--- portfolio/wp-content/themes/techis_自作テーマ/header.php
+++ portfolio/wp-content/themes/techis_自作テーマ/header.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Shippori+Mincho&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css">
 	<?php wp_head(); ?>
 </head>
+<div class="background-photo">
+	<div class="nav-group">
+		<div class="nav ff_cursive"><a href="<?php bloginfo('url'); ?>#home">Home</a></div>
+		<div class="nav ff_cursive"><a href="<?php bloginfo('url'); ?>#about">About</a></div>
+		<div class="nav ff_cursive"><a href="<?php bloginfo('url'); ?>#service">Service</a></div>
+		<div class="nav ff_cursive"><a href="<?php bloginfo('url'); ?>#works">Works</a></div>
+		<div class="nav ff_cursive"><a href="<?php bloginfo('url'); ?>#contact">Contact</a></div>
+	</div>
+	<div class="my-photo">
+		<img src="<?php bloginfo('template_url'); ?>/img/IMG_1150.JPG" alt="">
+		<p class="ff_cursive">@achaneko_914</p>
+		<a href=""><i class="fab fa-instagram"></i></a>
+	</div>
+</div>
+
 <body>
 	<header>
 	</header>
